05d69b67aa5cf05a88a461c617bbebd0c6b8d47c
Lakshay <ls199575@gmail.com>
2020-10-21 12:26:34 +0530

    search functionality completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movies from './Components/Movies';
+import SearchBar from './Components/SearchBar'
+import gif from '../src/eternity.gif'
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      data: [],
+      loading: false
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  searchMovie = async (text)=>{
+    const res = await (await fetch(`http://omdbapi.com/?apikey=bd992033&s=${text}`)).json()
+    this.setState({
+      data: res.Search,
+      loading: false
+    })
+    
+
+  }
+  clearData = () =>{
+    this.setState({
+      data: []
+    })
+  }
+  setLoadingTrue = () =>{
+    this.setState({
+      loading: true
+    })
+  }
+  
+  render(){
+    const gifPosition = {
+        position: "absolute",
+        margin: "0",
+        textAlign: "center",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)"
+    }
+    return (
+      <div className="container">
+        <SearchBar searchMovie={this.searchMovie} loading={this.setLoadingTrue}/>
+        
+       {!(this.state.data.length === 0) && <button className="btn btn-sm btn-danger" onClick={this.clearData} style={{width: "200px", marginTop: "20px"}}>Clear</button>}
+        
+        {!(this.state.loading) ? <Movies movies={this.state.data}/> : <img style={gifPosition} src={gif} alt={gif}/>}
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
